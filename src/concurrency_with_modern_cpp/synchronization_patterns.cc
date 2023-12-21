8c7b4482fd2f7905c9b2812851334163a8e54b6b
Yuanjun Ren <renyj@buaa.edu.cn>
2023-12-21 22:10:43 +0800

    Add ScopedLock testcases

--- src/concurrency_with_modern_cpp/synchronization_patterns.cc
+++ src/concurrency_with_modern_cpp/synchronization_patterns.cc
@@ -1,8 +1,11 @@
 #include <gtest/gtest.h>
 #include <functional>
 #include <iostream>
+#include <mutex>
+#include <new>
 #include <string>
 #include <thread>
+#include <utility>
 
 namespace data_race_with_reference_utils {
 
@@ -39,4 +42,52 @@ TEST(synchronization_patterns, test1) {
   t1.join();
   t2.join();
   t3.join();
+}
+
+namespace scoped_lock_utils {
+
+class ScopedLock {
+private:
+  std::mutex &mut;
+
+public:
+  explicit ScopedLock(std::mutex &m) : mut(m) {
+    mut.lock();
+    std::cout << "Lock the mutex: " << &mut << std::endl;
+  }
+
+  ~ScopedLock() {
+    std::cout << "Release the mutex: " << &mut << std::endl;
+    mut.unlock();
+  }
+};
+
+void test_scoped_lock() {
+  std::mutex mutex1;
+  ScopedLock scoped_lock1{mutex1};
+
+  std::cout << "\nBefore local scope" << std::endl;
+  {
+    std::mutex mutex2;
+    ScopedLock scoped_lock2{mutex2};
+  }
+  std::cout << "After local scope" << std::endl;
+
+  std::cout << "\nBefore try-catch block" << std::endl;
+  try {
+    std::mutex mutex3;
+    ScopedLock scoped_lock3{mutex3};
+    throw std::bad_alloc();
+  }
+  catch (std::bad_alloc &e) {
+    std::cout << e.what();
+  }
+  std::cout << "\nAfter try-catch block" << std::endl;
+}
+
+} // namespace scoped_lock_utils
+
+TEST(synchronization_patterns, test2) {
+  using namespace scoped_lock_utils;
+  test_scoped_lock();
 }
