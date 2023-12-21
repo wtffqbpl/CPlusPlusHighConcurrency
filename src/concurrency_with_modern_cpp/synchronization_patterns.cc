172967854cc956c6365c3356a4d603f0fcf185b2
Yuanjun Ren <renyj@buaa.edu.cn>
2023-12-21 23:09:53 +0800

    Add strategizedPolicy testcases

--- src/concurrency_with_modern_cpp/synchronization_patterns.cc
+++ src/concurrency_with_modern_cpp/synchronization_patterns.cc
@@ -1,9 +1,11 @@
 #include <gtest/gtest.h>
 #include <functional>
 #include <iostream>
+#include <memory>
 #include <mutex>
 #include <new>
 #include <string>
+#include <shared_mutex>
 #include <thread>
 #include <utility>
 
@@ -90,4 +92,155 @@ void test_scoped_lock() {
 TEST(synchronization_patterns, test2) {
   using namespace scoped_lock_utils;
   test_scoped_lock();
+}
+
+namespace strategy_pattern_utils {
+
+class Strategy {
+public:
+  virtual void operator()() = 0;
+  virtual ~Strategy() = default;
+};
+
+class Context {
+  std::shared_ptr<Strategy> _strat;
+
+public:
+  explicit Context() : _strat(nullptr) {}
+
+  void set_strategy(std::shared_ptr<Strategy> strat) { _strat = strat; }
+  void strategy() { if (_strat) (*_strat)(); }
+};
+
+
+class Strategy1 : public Strategy {
+  void operator()() override {
+    std::cout << "Foo" << std::endl;
+  }
+};
+
+class Strategy2 : public Strategy {
+  void operator()() override {
+    std::cout << "Bar" << std::endl;
+  }
+};
+
+class Strategy3 : public Strategy {
+  void operator()() override {
+    std::cout << "FooBar" << std::endl;
+  }
+};
+
+void test_strategy_policy() {
+  Context con;
+
+  con.set_strategy(std::shared_ptr<Strategy>(new Strategy1));
+  con.strategy();
+
+  con.set_strategy(std::shared_ptr<Strategy>(new Strategy2));
+  con.strategy();
+
+  con.set_strategy(std::shared_ptr<Strategy>(new Strategy3));
+  con.strategy();
+};
+
+} // namespace strategy_pattern_utils
+
+TEST(synchronization_patterns, test3) {
+  using namespace strategy_pattern_utils;
+
+  test_strategy_policy();
+}
+
+namespace strategized_locking_runtime {
+
+class Lock {
+public:
+  virtual void lock() const = 0;
+  virtual void unlock() const = 0;
+};
+
+class StrategizedLocking {
+  Lock &lock;
+
+public:
+  StrategizedLocking(Lock &l) : lock(l) {
+    lock.lock();
+  }
+
+  ~StrategizedLocking() {
+    lock.unlock();
+  }
+};
+
+struct NullObjectMutex {
+  void lock() {}
+  void unlock() {}
+};
+
+class NoLock : public Lock {
+  void lock() const override {
+    std::cout << "NoLock::lock: " << std::endl;
+    null_object_mutex.lock();
+  }
+
+  void unlock() const override {
+    std::cout << "NoLock::unlock: " << std::endl;
+    null_object_mutex.unlock();
+  }
+
+private:
+  mutable NullObjectMutex null_object_mutex;
+};
+
+class ExclusiveLock :public Lock {
+  void lock() const override {
+    std::cout << "    ExclusiveLock::lock: " << std::endl;
+    mtx.lock();
+  }
+
+  void unlock() const override {
+    std::cout << "    ExclusiveLock::unlock: " << std::endl;
+    mtx.unlock();
+  }
+
+private:
+  mutable std::mutex mtx;
+};
+
+class SharedLock : public Lock {
+  void lock() const override {
+    std::cout << "        SharedLock::lock_shared: " << std::endl;
+    shared_mtx.lock();
+  }
+
+  void unlock() const override {
+    std::cout << "        SharedLock::unlock_shared: " << std::endl;
+    shared_mtx.unlock();
+  }
+
+private:
+  mutable std::shared_mutex shared_mtx;
+};
+
+void test_strategized_locking() {
+  NoLock no_lock;
+  StrategizedLocking strat_lock1{no_lock};
+
+  {
+    ExclusiveLock ex_lock;
+    StrategizedLocking strat_lock2{ex_lock};
+    {
+      SharedLock shar_lock;
+      StrategizedLocking strat_lock3{shar_lock};
+    }
+  }
+}
+
+} // namespace strategized_locking_runtime
+
+TEST(synchronization_patterns, test4) {
+  using namespace strategized_locking_runtime;
+
+  test_strategized_locking();
 }
